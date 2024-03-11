49d4fb548fa51aefd9a52d2123d2635d5be65aca
Dorina24 <euseidm@gmail.com>
2024-03-11 19:46:25 +0200

    IF

--- script.js
+++ script.js
@@ -27,3 +27,30 @@ else
     if(!hasEaten)
     {console.log('Inca nu ai terminat de mancat! Te poti uita la DA dupa ce farfuria este goala')} 
 }
+
+//   IF - ELSE IF - ELSE ___________________________________
+let number = -13;
+if (number >0) {
+    console.log('Numarul este pozitiv.')
+}     
+      else if (number<0)  { 
+        console.log("Numarul ales este negativ.")
+       }   
+            else{
+                console.log('Numarul ales este zero')
+            }
+
+//SWITCH__________________________
+
+const dayOfTheWeek = 2;
+switch (dayOfTheWeek)
+{ case 1:
+       console.log('Ziua este luni');
+        break;
+  case 2:
+        console.log('Ziua saptamanii este marti.')
+        break; 
+  default:
+        console.log('Nu stiu ce zi a saptamanii este aceasta!');
+        break;    
+}
